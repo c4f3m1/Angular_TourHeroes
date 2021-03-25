6719cae27cd5dfa01db7282a95299a4fd8121159
c4f3m1 <c4f3m1.on@gmail.com>
2021-03-25 10:33:45 +0100

    injection services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   //Sincrono
   /*getHeroes(): Hero[] {
@@ -17,6 +18,8 @@ export class HeroService {
 
   //Asincrono
   getHeroes(): Observable<Hero[]> {
+      // TODO: send the message _after_ fetching the heroes
+      this.messageService.add('HeroService: fetched heroes');
       return of(HEROES);
   }
 
